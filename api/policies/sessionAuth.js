d77e3627fe6de609744f22f978be835afd2c3faa
Oswaldo Avalos <oavalos@agari.com>
2015-06-18 12:05:47 -0700

    Minor details

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -14,8 +14,9 @@ module.exports = function(req, res, next) {
   if (req.session.authenticated) {
     return next();
   }
+  res.redirect('/login');
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  //return res.forbidden('You are not permitted to perform this action.');
 };
